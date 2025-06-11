6d824da8b38e0884e6bd0c9baddda14d7a6b168f
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2025-06-11 10:51:20 -0400

    List of tasks to implement

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -145,6 +145,23 @@ void AudiopluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, 
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
+    // TODO: add APVTS
+    // TODO: create audio parameters for all dsp choices
+    // TODO: update DSP ahere from audio parameters
+    // TODO: save/load settings
+    // TODO: save/load DSP order
+    // TODO: Drag-To-Reorder GUI
+    // TODO: GUI design for each DSP instance?
+    // TODO: metering
+    // TODO: prepare all DSP
+    // TODO: wet/dry knob [BONUS]
+    // TODO: mono & stero versions [mono is BONUS]
+    // TODO: modulators [BONUS]
+    // TODO: thread-safe filter updating [BONUS]
+    // TODO: pre/post filtering [BONUS]
+    // TODO: delay module [BONUS]
+
+
     auto newDSPOrder = DSP_Order();
 
     // try to pull
